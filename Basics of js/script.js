2ec9c11633f790831459ecb871b35617728054d0
vikram2801 <vikrammuniyappan8@gmail.com>
2026-08-27 14:30:29 +0530

    basic code

---
+++ Basics of js/script.js
@@ -0,0 +1,27 @@
+// Create a variable for student name, age, and mark and print all three.
+
+var studentName = "vikram";
+let age = 25;
+const mark = 97
+
+console.log(studentName,age,mark);
+
+
+// Ask the user's name using prompt() and display it using alert().
+
+let name = prompt("Enter your name:");
+alert("Hello " + name);
+
+// Ask the user's age using prompt() and print it using console.log().
+
+const userAge = prompt("Enter your age");
+console.log("userAge:",userAge)
+
+// Ask the user a question using confirm().
+
+let askQuestion = confirm("Are you sure delete ?");
+console.log(askQuestion);
+
+//Ask the user's name and display it on the webpage using document.writeln().
+var city =  prompt('enter your city')
+document.writeln("Welcome to " + city);
