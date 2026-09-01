a2549a918b9651849297c8d08dff38bf9dd62106
vikram2801 <vikrammuniyappan8@gmail.com>
2026-09-01 10:56:27 +0530

    task2 code completed

--- task2/task2.js
+++ task2/task2.js
@@ -40,11 +40,11 @@ console.log(studentName);  // Ram
 // City
 // Print the result in the console.
 
-var name = prompt("Enter your Name");
+var name11 = prompt("Enter your Name");
 let age = prompt("Enter your Age");
 const city = prompt("Enter your City");
 
-console.log("Name:",name);
+console.log("Name:",name11);
 console.log("Age:",age);
 console.log("City:",city);
 
@@ -99,10 +99,10 @@ console.log(studentData.isStudent);
 // Task 7 — Fruit Array
 
 let fruit =["Apple","Mango","Orange","Banana","Grapes","Papaya"];
-console.log("First fruit:", fruits[0]);
-console.log("Second fruit:", fruits[1]);
-console.log("Last fruit:", fruits[fruits.length - 1]);
-console.log("Total number of fruits:", fruits.length);
+console.log("First fruit:", fruit[0]);
+console.log("Second fruit:", fruit[1]);
+console.log("Last fruit:", fruit[fruit.length - 1]);
+console.log("Total number of fruits:", fruit.length);
 
 // Arithmetic Operator Tasks
 // Task 8 — Basic Calculator
@@ -137,13 +137,129 @@ let averageMarks = TotalMarks /100
 console.log("Total marks",TotalMarks);
 console.log("Average Mark",averageMarks);
 
+// Task 11 — Post Increment
+let a2 = 10;
+
+let b2 = a2++;
+
+console.log(a2); //11
+console.log(b2); //10
 
 
+// Task 12 — Pre Increment
+
+let a3 = 10;
+
+let b3 = ++a3;
+
+console.log(a3); //11
+console.log(b3); //11
  
+// Task 13 — Post Decrement
+
+let a4 = 20;
+
+let b4 = a4--;
+
+console.log(a);//19
+console.log(b);//20
+
+// Task 14 — Pre Decrement
 
 
+let a5 = 20;
+
+let b5 = --a5;
+
+console.log(a5); //19
+console.log(b5);//19
+
+// Task 15 — Find the Final Values
 
 
+let a6 = 5;
+
+let b6 = a++;
+
+let c6 = ++a;
+
+let d6 = b--;
+
+console.log(a6); //7
+console.log(b6); //4 
+console.log(c6); //7
+console.log(d6); //5
+
+
+//Task 16 — Assignment Operators
+
+let num = 10;
+num +=5;
+console.log(num); //15
+
+let num1 = 10;
+num1 -=5;
+console.log(num1); // 5
+
+let num2 = 10;
+num2 *=5;
+console.log(num2); //50
+
+let num3 =10;
+num3 /=5;
+console.log(num3); //2
+
+let num4 = 10;
+num4 %= 3
+console.log(num4); //1
+
+let num5 =10;
+num5 **= 2
+console.log(num5); //100
+
+
+//Task 17 — Mini Student Profile
+
+//variables
+let name = "Vikram";
+let age1 = 22;
+let city1 = "Coimbatore";
+let college = "GCT College";
+
+//Array
+
+let favSubject = ["HTML","CSS","Javascript","Node","Python"] ;
+
+//object
+
+let studentProf ={
+    "name":"vikram",
+    "age":23,
+    "city":"coimbatore",
+    "subjects": favSubject,
+    "isStudent":true
+}
+
+console.log("Student Name:", studentProf.name);
+console.log("Student Age:", studentProf.age);
+console.log("City:", studentProf.city);
+console.log("First Subject:", studentProf.subjects[0]);
+console.log("Last Subject:", studentProf.subjects[studentProf.subjects.length - 1]);
+console.log("Total Subjects:", studentProf.subjects.length);
+console.log("Complete Object:", studentProf);
+
+
+//Final Task
+
+let num6 = Number(prompt("Enter first number:"));
+let num7 = Number(prompt("Enter second number:"));
+
+console.log("Addition:", num6 + num7);
+console.log("Subtraction:", num6 - num7);
+console.log("Multiplication:", num6 * num7);
+console.log("Division:", num6 / num7);
+console.log("Modulus:", num6 % num7);
+console.log("Power:", num6 ** num7);
 
 
 
